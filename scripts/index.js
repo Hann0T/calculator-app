665dbf48b8db8217b4b712d5964bbe9cf11d6e5a
Hans <hansolivas1@gmail.com>
2021-08-27 12:14:52 -0500

    Some minor changes to css & html, the js code needs to be refactorized

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
 const keyNumbers = document.querySelectorAll("#key__number");
+const keyOperations = document.querySelectorAll(".calculator__key--operator");
 const keyDelete = document.querySelector(".calculator__key--delete");
+const keyEquals = document.querySelector(".calculator__key--equal");
 const keyReset = document.querySelector(".calculator__key--reset");
+const keyDot = document.querySelector(".calculator__key--dot");
 const screen = document.querySelector(".calculator__screen--text");
+const overheadScreen = screen.previousElementSibling;
+
+const screenValues = {
+    prevValue: 0,
+    nextValue: 0,
+    operationToDo: "",
+};
 
 keyNumbers.forEach(function (key) {
     key.addEventListener("click", () => {
@@ -22,4 +32,51 @@ keyDelete.addEventListener("click", () => {
 
 keyReset.addEventListener("click", () => {
     screen.textContent = 0;
+    overheadScreen.textContent = "";
 });
+
+keyDot.addEventListener("click", () => {
+    screen.textContent += ".";
+});
+
+keyOperations.forEach((key) => {
+    key.addEventListener("click", () => {
+        screenValues.prevValue = parseFloat(screen.textContent);
+        screenValues.operationToDo = key.textContent.toLowerCase();
+        overheadScreen.textContent = `${screen.textContent}${key.textContent}`;
+        screen.textContent = 0;
+    });
+});
+
+keyEquals.addEventListener("click", () => {
+    screenValues.nextValue = parseFloat(screen.textContent);
+    overheadScreen.textContent += screen.textContent + "=";
+    let { prevValue, nextValue, operationToDo } = screenValues;
+    const operator = matchOperator(operationToDo);
+    screen.textContent = operator(prevValue, nextValue);
+});
+
+function matchOperator(operator) {
+    switch (operator) {
+        case "+": {
+            return (a, b) => {
+                return a + b;
+            };
+        }
+        case "-": {
+            return (a, b) => {
+                return a - b;
+            };
+        }
+        case "x": {
+            return (a, b) => {
+                return a * b;
+            };
+        }
+        case "/": {
+            return (a, b) => {
+                return a / b;
+            };
+        }
+    }
+}
