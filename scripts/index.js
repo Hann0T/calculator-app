c5ce397d93bf9ea930b0a870d117095224d03951
Hans <hansolivas1@gmail.com>
2021-09-21 07:33:57 -0500

    something

--- scripts/index.js
+++ scripts/index.js
@@ -15,71 +15,71 @@ const keyEquals = document.querySelector(".calculator__key--equal");
 const calculator = new Calculator(screen, overheadScreen);
 
 keyNumbers.forEach((key) => {
-    key.addEventListener("click", () => {
-        calculator.displayInScreen(key.textContent);
-    });
+  key.addEventListener("click", () => {
+    calculator.displayInScreen(key.textContent);
+  });
 });
 keyReset.addEventListener("click", () => {
-    calculator.clear();
+  calculator.clear();
 });
 keyDelete.addEventListener("click", () => {
-    calculator.deleteNumber();
+  calculator.deleteNumber();
 });
 keyDot.addEventListener("click", () => {
-    calculator.displayInScreen(keyDot.textContent);
+  calculator.displayInScreen(keyDot.textContent);
 });
 keyOperations.forEach((key) => {
-    key.addEventListener("click", () => {
-        calculator.state = {
-            ...calculator.state,
-            firstNumber: parseFloat(screen.textContent),
-            operator: key.textContent,
-        };
-        calculator.displayInTopScreen(
-            `${calculator.state.firstNumber}${calculator.state.operator}`
-        );
-    });
+  key.addEventListener("click", () => {
+    calculator.state = {
+      ...calculator.state,
+      firstNumber: parseFloat(screen.textContent),
+      operator: key.textContent,
+    };
+    calculator.displayInTopScreen(
+      `${calculator.state.firstNumber}${calculator.state.operator}`
+    );
+  });
 });
 keyEquals.addEventListener("click", () => {
-    calculator.state = {
-        ...calculator.state,
-        secondNumber: parseFloat(screen.textContent),
-    };
-    calculator.calculate();
+  calculator.state = {
+    ...calculator.state,
+    secondNumber: parseFloat(screen.textContent),
+  };
+  calculator.calculate();
 });
 
 const setTheme = (value) => {
-    localStorage.setItem("theme", value);
-    switch (localStorage.getItem("theme")) {
-        case "1": {
-            themeSwitcher.dataset.theme = "theme-one";
-            changeTheme(themeSwitcher.dataset.theme);
-            break;
-        }
-        case "2": {
-            themeSwitcher.dataset.theme = "theme-two";
-            changeTheme(themeSwitcher.dataset.theme);
-            break;
-        }
-        default: {
-            themeSwitcher.dataset.theme = "theme-three";
-            changeTheme(themeSwitcher.dataset.theme);
-        }
+  localStorage.setItem("theme", value);
+  switch (localStorage.getItem("theme")) {
+    case "1": {
+      themeSwitcher.dataset.theme = "theme-one";
+      changeTheme(themeSwitcher.dataset.theme);
+      break;
     }
+    case "2": {
+      themeSwitcher.dataset.theme = "theme-two";
+      changeTheme(themeSwitcher.dataset.theme);
+      break;
+    }
+    default: {
+      themeSwitcher.dataset.theme = "theme-three";
+      changeTheme(themeSwitcher.dataset.theme);
+    }
+  }
 };
 
 const changeTheme = (theme) => {
-    app.removeAttribute("class");
-    app.classList.add("app");
-    app.classList.add(theme);
+  app.removeAttribute("class");
+  app.classList.add("app");
+  app.classList.add(theme);
 };
 
 if (!localStorage.getItem("theme")) {
-    localStorage.setItem("theme", 1);
+  localStorage.setItem("theme", 1);
 }
 themeSwitcher.value = localStorage.getItem("theme");
 setTheme(themeSwitcher.value);
 
 themeSwitcher.addEventListener("change", (e) => {
-    setTheme(e.target.value);
+  setTheme(e.target.value);
 });
